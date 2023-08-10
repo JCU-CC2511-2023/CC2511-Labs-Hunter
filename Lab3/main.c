7023473763b80e845a0d67bf95e7f41c5fa7f242
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-10 15:53:07 +1000

    Lab 3 -- Complete
    Code Completed and functional
    Red, Blue and Green LEDs are toggleable by keypress

--- Lab3/main.c
+++ Lab3/main.c
@@ -13,21 +13,44 @@ int main(void) {
   // TODO - Initialise components and variables
   stdio_init_all(); //Bruce put this here, so im fairly sure its useful
   printf("Hello, World!\r\n"); //test print
-  //*#define RED_LED 11 // these are hopefully the correct numbers
-  //*#define GREEN_LED 12
-  //*#define BLUE_LED 13
 
-  //*gpio_init(RED_LED);
-  //*gpio_init(GREEN_LED);
-  //*gpio_init(BLUE_LED);
+
+   
+  
+  
+  #define RED_LED 11
+  gpio_init(RED_LED);
+  gpio_set_dir(RED_LED, true);
+
+  #define GREEN_LED 12
+  gpio_init(GREEN_LED);
+  gpio_set_dir(GREEN_LED, true);
+
+  #define BLUE_LED 13
+  gpio_init(BLUE_LED);
+  gpio_set_dir(BLUE_LED, true);
 
   while (true) {
     // TODO - Repeated code here
-
     //respond to user input by setting the GPIO pin high or low
-    //*gpio_set_dir(RED_LED, true);
-    //*gpio_set_dir(BLUE_LED, true);
-    //*gpio_set_dir(RED_LED, true);
-    printf("Some text\r\n");
+    
+    int ch = getchar_timeout_us(0);
+    if (ch != PICO_ERROR_TIMEOUT) {
+    // Handle the various interesting values of ch here...
+      if (ch == 'r'){
+        gpio_put(RED_LED, !gpio_get(RED_LED));//inverses current value of the GPIO pin
+        printf("RED TOGGLED\r\n");
+        }
+      if (ch == 'g'){
+        gpio_put(GREEN_LED, !gpio_get(GREEN_LED));//inverses current value of the GPIO pin
+        printf("GREEN TOGGLED\r\n");
+        }
+      if (ch == 'b'){
+        gpio_put(BLUE_LED, !gpio_get(BLUE_LED));//inverses current value of the GPIO pin
+        printf("BLUE TOGGLED\r\n");
+        }  
+    }
+    
+    sleep_ms(20);//wait for another key press
   }
 }
