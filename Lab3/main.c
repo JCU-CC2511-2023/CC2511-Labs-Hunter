733c7e5f5f1eb494f95fc8aa8d5a5cee4e89ee85
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-10 12:10:48 +1000

    initial commit
    creation of base files

---
+++ Lab3/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 10-Aug-2023 hkrug
+ * Lab3
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
