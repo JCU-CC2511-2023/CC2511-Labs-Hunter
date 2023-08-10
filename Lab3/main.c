2d79cd14387f9594a15d25a02ee0d56a04a5846e
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-10 15:12:02 +1000

    Serial Port Connected with arbitrary output
    the commented out code is the next section, which i started, however it is a good idea to verify that the serial port will work and the starter code is fully functional.

--- Lab3/main.c
+++ Lab3/main.c
@@ -6,11 +6,28 @@
 
 #include "pico/stdlib.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 
 int main(void) {
   // TODO - Initialise components and variables
+  stdio_init_all(); //Bruce put this here, so im fairly sure its useful
+  printf("Hello, World!\r\n"); //test print
+  //*#define RED_LED 11 // these are hopefully the correct numbers
+  //*#define GREEN_LED 12
+  //*#define BLUE_LED 13
+
+  //*gpio_init(RED_LED);
+  //*gpio_init(GREEN_LED);
+  //*gpio_init(BLUE_LED);
+
   while (true) {
     // TODO - Repeated code here
+
+    //respond to user input by setting the GPIO pin high or low
+    //*gpio_set_dir(RED_LED, true);
+    //*gpio_set_dir(BLUE_LED, true);
+    //*gpio_set_dir(RED_LED, true);
+    printf("Some text\r\n");
   }
 }
