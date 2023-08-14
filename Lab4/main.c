921ceb9ec7b60521f76d4c35363e54f1b4668bff
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-14 15:57:25 +1000

    Lab4 main c file created

---
+++ Lab4/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 14-Aug-2023 Hunter Kruger-Ilingworth
+ * Lab4
+ * objective: control onboard LED without using the SDK.
+ * ***********************************************************/
+#include <stdbool.h>
+
+int main(void) {
+  // 1. enable the pads
+  // 2. select the GPIO function
+  // 3. set the data direction.
+  while (true) {
+    //  code to turn the LED on and off, with a timed waiting period between each action.
+  }
+}
