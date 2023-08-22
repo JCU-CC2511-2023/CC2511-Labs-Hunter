e1121e0948ed15a2ccfe8c029249714eca5eea9e
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-22 12:38:50 +1000

    Pullup/Pulldown Resistor implementation
    it was confirmed the presence of the pullup or pulldown resistor avoids
    the unpredictable behaviour that was observed earlier.

--- Lab5/main.c
+++ Lab5/main.c
@@ -3,7 +3,7 @@
  * rev 1.0 21-Aug-2023 Hunter Kruger-Ilingworth
  * Lab5
  * ***********************************************************/
-
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include <stdbool.h>
 #include "hardware/pwm.h"
@@ -21,6 +21,7 @@ int main(void)
 {
   // ENABLE USE OF SDK
   stdio_init_all();
+
   // INITIALISE BLUE_LED
   gpio_init(BLUE_LED);
   gpio_set_dir(BLUE_LED, true); // recieving input
@@ -33,6 +34,38 @@ int main(void)
   while (true)
   {
     // use blue led to display the status of the chosen pin
-    gpio_put(BLUE_LED, !gpio_get(FLOATING_PIN));
+    gpio_put(BLUE_LED, gpio_get(FLOATING_PIN));
+
+    int ch = getchar_timeout_us(0);
+    if (ch != PICO_ERROR_TIMEOUT) // code to detect character input
+    {
+      // Handle the various interesting values of ch here...
+      if (ch == 'u')
+      {
+        // If you receive ’u’ turn the pullup on and the pulldown off.
+        CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // clear both
+        CONTENT_OF(REG_PAD_CONTROL_GPIO15) |= CTRL_PUE;               // set PUE to 1
+        printf("Pin %d to Pullup\r\n", FLOATING_PIN);
+      }
+      if (ch == 'd')
+      {
+        // If you receive ’d’ turn the pullup off and the pulldown on.
+        CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // clear both
+        CONTENT_OF(REG_PAD_CONTROL_GPIO15) |= CTRL_PDE;               // set PDE to 1
+        printf("Pin %d to Pulldown\r\n", FLOATING_PIN);
+      }
+      if (ch == 'o')
+      {
+        // If you receive ’o’ turn both pullup and pulldown off.
+        CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // clear both
+        printf("Pin %d to Pull none\r\n", FLOATING_PIN);
+      }
+      if (ch == 'h') //using an else statment was buggy for some reason
+      {
+        //intruction print for incorrect keypresses (h for help)
+        printf("Pin %d Options:\r\n u - Pullup \r\n d - Pulldown \r\n o - Pull none \r\n", FLOATING_PIN);
+      }
+    }
+    sleep_ms(20); //buffer for CPU
   }
 }
