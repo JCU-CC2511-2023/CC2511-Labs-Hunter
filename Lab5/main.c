65a93697a85b4a7806da0a2bf457565fc49adbf9
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-22 15:33:38 +1000

    part 1 removed, part 2 working
    part 2 implemented, with the functionality of part 1 removed for ease of development

--- Lab5/main.c
+++ Lab5/main.c
@@ -8,64 +8,52 @@
 #include <stdbool.h>
 #include "hardware/pwm.h"
 
-#define FLOATING_PIN 15
+#define RED_LED 11
+#define GREEN_LED 12
 #define BLUE_LED 13
-
-#define CONTENT_OF(addr) (*(volatile uint32_t *)addr)
-#define PAD_CONTROL_BASE 0x4001c000
-#define REG_PAD_CONTROL_GPIO15 (PAD_CONTROL_BASE + 0x40)
-#define CTRL_PDE 0x4 // Bit 2 REG_PAD_CONTROL_GPIO15 register pg300 rp2040 datasheet
-#define CTRL_PUE 0x8 // Bit 3 REG_PAD_CONTROL_GPIO15 register pg300 rp2040 datasheet
 
 int main(void)
 {
   // ENABLE USE OF SDK
   stdio_init_all();
 
-  // INITIALISE BLUE_LED
-  gpio_init(BLUE_LED);
-  gpio_set_dir(BLUE_LED, true); // recieving input
-  // INITIALISE FLOATING PIN
-  gpio_init(FLOATING_PIN);
-  gpio_set_dir(FLOATING_PIN, false); // outputting signal
+  // INITIALISE LED PINS
+  gpio_init(RED_LED);
+  gpio_set_dir(RED_LED, true);               // recieving input
+  gpio_set_function(RED_LED, GPIO_FUNC_PWM); // set function to PWM
+  uint Rslice_num = pwm_gpio_to_slice_num(RED_LED);
+  pwm_set_wrap(Rslice_num, 255); // may have to adjust
+  pwm_set_chan_level(Rslice_num, PWM_CHAN_A, 128);
+  pwm_set_enabled(Rslice_num, true);
+  //pwm_set_gpio_level(RED_LED, 255);
 
-  // add code to clear the PUE and PDE registers
-  CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // resets CTRL_PUE and CTRL_PDE pins
+  gpio_init(GREEN_LED);
+  gpio_set_dir(GREEN_LED, true);               // recieving input
+  gpio_set_function(GREEN_LED, GPIO_FUNC_PWM); // set function to PWM
+  uint Gslice_num = pwm_gpio_to_slice_num(GREEN_LED);
+  pwm_set_wrap(Gslice_num, 255); // may have to adjust
+  pwm_set_chan_level(Gslice_num, PWM_CHAN_A, 128);
+  pwm_set_enabled(Gslice_num, true);
+  pwm_set_gpio_level(GREEN_LED, 255);
+
+  gpio_init(BLUE_LED);
+  gpio_set_dir(BLUE_LED, true);               // recieving input
+  gpio_set_function(BLUE_LED, GPIO_FUNC_PWM); // set function to PWM
+  uint Bslice_num = pwm_gpio_to_slice_num(BLUE_LED);
+  pwm_set_wrap(Bslice_num, 255); // may have to adjust
+  pwm_set_chan_level(Bslice_num, PWM_CHAN_A, 128);
+  pwm_set_enabled(Bslice_num, true);
+  pwm_set_gpio_level(BLUE_LED, 255);
+
   while (true)
   {
-    // use blue led to display the status of the chosen pin
-    gpio_put(BLUE_LED, gpio_get(FLOATING_PIN));
-
-    int ch = getchar_timeout_us(0);
-    if (ch != PICO_ERROR_TIMEOUT) // code to detect character input
+    // Adjust the duty cycle of the green LED to create blinking effect
+    for (int duty_cycle = 0; duty_cycle <= 255; duty_cycle += 1)
     {
-      // Handle the various interesting values of ch here...
-      if (ch == 'u')
-      {
-        // If you receive ’u’ turn the pullup on and the pulldown off.
-        CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // clear both
-        CONTENT_OF(REG_PAD_CONTROL_GPIO15) |= CTRL_PUE;               // set PUE to 1
-        printf("Pin %d to Pullup\r\n", FLOATING_PIN);
-      }
-      if (ch == 'd')
-      {
-        // If you receive ’d’ turn the pullup off and the pulldown on.
-        CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // clear both
-        CONTENT_OF(REG_PAD_CONTROL_GPIO15) |= CTRL_PDE;               // set PDE to 1
-        printf("Pin %d to Pulldown\r\n", FLOATING_PIN);
-      }
-      if (ch == 'o')
-      {
-        // If you receive ’o’ turn both pullup and pulldown off.
-        CONTENT_OF(REG_PAD_CONTROL_GPIO15) &= ~(CTRL_PDE + CTRL_PUE); // clear both
-        printf("Pin %d to Pull none\r\n", FLOATING_PIN);
-      }
-      if (ch == 'h') //using an else statment was buggy for some reason
-      {
-        //intruction print for incorrect keypresses (h for help)
-        printf("Pin %d Options:\r\n u - Pullup \r\n d - Pulldown \r\n o - Pull none \r\n", FLOATING_PIN);
-      }
-    }
-    sleep_ms(20); //buffer for CPU
+        pwm_set_gpio_level(RED_LED, duty_cycle);
+        pwm_set_gpio_level(GREEN_LED, duty_cycle);
+        pwm_set_gpio_level(BLUE_LED, duty_cycle);
+        sleep_ms(20); // Delay to observe changes
+    }   
   }
 }
