27a6bae81b541f803c9af779ce16242219f2f7ad
H-unter <99230734+H-unter@users.noreply.github.com>
2023-08-30 15:22:34 +1000

    lab5 extension adjustment
    20ms -> 2ms

--- Lab5sinewave/main.c
+++ Lab5sinewave/main.c
@@ -65,7 +65,7 @@ int main(void)
       pwm_set_gpio_level(GREEN_LED, (int)green_value);
       pwm_set_gpio_level(BLUE_LED, (int)blue_value);
 
-      sleep_ms(20); // Delay to observe changes
+      sleep_ms(2); // Delay to observe changes
     }   
   }
 }
