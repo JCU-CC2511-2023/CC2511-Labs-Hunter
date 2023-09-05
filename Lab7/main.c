5339ab4f0412568ee34f794331245618490658a5
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-05 14:00:28 +1000

    lab7 initial commit

---
+++ Lab7/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 05-Sep-2023 hkrug
+ * Lab7
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include "hardware/uart.h"
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
